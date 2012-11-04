6f1855b82d31c4b3ec7cd9b0e31d314ffba31a08
Andrew Won <andyjwon@gmail.com>
2012-11-04 12:08:25 -0800

    Created Item domain object and accompanying unit tests

---
+++ service/src/main/java/edu/lmu/cs/diabolical/ws/domain/Item.java
@@ -0,0 +1,157 @@
+package edu.lmu.cs.diabolical.ws.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * An item is equippable by a character and provides changes to charstats.
+ */
+@Entity
+@XmlRootElement
+public class Item {
+
+    private Long id;
+    private String slot;
+    private Double mindamage;
+    private Double maxdamage;
+    private Double critchance;
+    private Integer atkspeed;
+    private Double defense;
+    private Double absorption;
+    private Double blockchance;
+
+    public Item() {
+        // No-arg constructor
+    }
+
+    public Item(Long id, String slot, Double mindamage, Double maxdamage, Double critchance, Integer atkspeed,
+            Double defense, Double absorption, Double blockchance) {
+        this.id = id;
+        this.slot = slot;
+        this.mindamage = mindamage;
+        this.maxdamage = maxdamage;
+        this.critchance = critchance;
+        this.atkspeed = atkspeed;
+        this.defense = defense;
+        this.absorption = absorption;
+        this.blockchance = blockchance;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @XmlAttribute
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Lob
+    public String getSlot() {
+        return slot;
+    }
+
+    public void setSlot(String slot) {
+        this.slot = slot;
+    }
+
+    public Double getMindamage() {
+        return mindamage;
+    }
+
+    public void setMindamage(Double mindamage) {
+        this.mindamage = mindamage;
+    }
+
+    public Double getMaxdamage() {
+        return maxdamage;
+    }
+
+    public void setMaxdamage(Double maxdamage) {
+        this.maxdamage = maxdamage;
+    }
+
+    public Double getCritchance() {
+        return critchance;
+    }
+
+    public void setCritchance(Double critchance) {
+        this.critchance = critchance;
+    }
+
+    public Integer getAtkspeed() {
+        return atkspeed;
+    }
+
+    public void setAtkspeed(Integer atkspeed) {
+        this.atkspeed = atkspeed;
+    }
+
+    public Double getDefense() {
+        return defense;
+    }
+
+    public void setDefense(Double defense) {
+        this.defense = defense;
+    }
+
+    public Double getAbsorption() {
+        return absorption;
+    }
+
+    public void setAbsorption(Double absorption) {
+        this.absorption = absorption;
+    }
+
+    public Double getBlockchance() {
+        return blockchance;
+    }
+
+    public void setBlockchance(Double blockchance) {
+        this.blockchance = blockchance;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{id: " + id + ", slot: " + slot + ", mindamage: " + mindamage + ", maxdamage: " + maxdamage
+                + ", critchance: " + critchance + ", atkspeed: " + atkspeed + ", defense: " + defense
+                + ", absorption: " + absorption + ", blockchance: " + blockchance + "}";
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        Item other = (Item) obj;
+        if (id == null) {
+            if (other.id != null) {
+                return false;
+            }
+        } else if (!id.equals(other.id)) {
+            return false;
+        }
+        return true;
+    }
+}
---
+++ service/src/test/java/edu/lmu/cs/diabolical/ws/domain/ItemTest.java
@@ -0,0 +1,85 @@
+package edu.lmu.cs.diabolical.ws.domain;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertFalse;
+
+import org.junit.Test;
+
+public class ItemTest {
+    @Test
+    public void fieldsSetByConstructorCanBeRead() {
+        Item item = new Item(5L, "Bracer", 1600.0, 1450.0, 0.90, 2, 99.9, 99.8, 99.7);
+        assertThat(item.getId(), is(5L));
+        assertThat(item.getSlot(), is("Bracer"));
+        assertThat(item.getMindamage(), is(1600.0));
+        assertThat(item.getMaxdamage(), is(1450.0));
+        assertThat(item.getCritchance(), is(0.9));
+        assertThat(item.getAtkspeed(), is(2));
+        assertThat(item.getDefense(), is(99.9));
+        assertThat(item.getAbsorption(), is(99.8));
+        assertThat(item.getBlockchance(), is(99.7));
+    }
+
+    @Test
+    public void fieldsSetBySettersCanBeRead() {
+        Item item = new Item();
+        item.setId(5L);
+        item.setSlot("Bracer");
+        item.setMindamage(1600.0);
+        item.setMaxdamage(1450.0);
+        item.setCritchance(0.90);
+        item.setAtkspeed(2);
+        item.setDefense(99.9);
+        item.setAbsorption(99.8);
+        item.setBlockchance(99.7);
+        assertThat(item.getId(), is(5L));
+        assertThat(item.getSlot(), is("Bracer"));
+        assertThat(item.getMindamage(), is(1600.0));
+        assertThat(item.getMaxdamage(), is(1450.0));
+        assertThat(item.getCritchance(), is(0.9));
+        assertThat(item.getAtkspeed(), is(2));
+        assertThat(item.getDefense(), is(99.9));
+        assertThat(item.getAbsorption(), is(99.8));
+        assertThat(item.getBlockchance(), is(99.7));
+    }
+
+    @Test
+    public void toStringProducesExpectedString() {
+        Item item1 = new Item(5L, "Bracer", 1600.0, 1450.0, 0.90, 2, 99.9, 99.8, 99.7);
+        String expected1 = "Item{id: 5, slot: Bracer, mindamage: 1600.0, maxdamage: 1450.0, critchance: 0.9, atkspeed: 2, defense: 99.9, absorption: 99.8, blockchance: 99.7}";
+        Item item2 = new Item();
+        item2.setId(5L);
+        item2.setSlot("Helm");
+        item2.setMindamage(1600.0);
+        item2.setMaxdamage(1599.0);
+        item2.setCritchance(0.90);
+        item2.setAtkspeed(2);
+        item2.setDefense(99.9);
+        item2.setAbsorption(99.8);
+        item2.setBlockchance(100.0);
+        String expected2 = "Item{id: 5, slot: Helm, mindamage: 1600.0, maxdamage: 1599.0, critchance: 0.9, atkspeed: 2, defense: 99.9, absorption: 99.8, blockchance: 100.0}";
+        assertThat(item1.toString(), is(expected1));
+        assertThat(item2.toString(), is(expected2));
+    }
+
+    @Test
+    public void hashCodeProducesPrimePlusId() {
+        // Where prime is set as 31
+        assertThat(new Item(5L, "Bracer", 1600.0, 1450.0, 0.90, 2, 99.9, 99.8, 99.7).hashCode(), is(36));
+    }
+
+    @Test
+    public void equalsUsesIdOnly() {
+        assertThat(new Item(5L, "Bracer", 1600.0, 1450.0, 0.90, 2, 99.9, 99.8, 99.7), equalTo(new Item(5L, "Not", 100.0, 140.0, 0.20, 1, 9.9, 9.8, 9.7)));
+        assertThat(new Item(5L, "Bracer", 1600.0, 1450.0, 0.90, 2, 99.9, 99.8, 99.7),
+                not(equalTo(new Item(7L, "Bracer", 1600.0, 1450.0, 0.90, 2, 99.9, 99.8, 99.7))));
+
+        assertFalse(new Item(5L, "Bracer", 1600.0, 1450.0, 0.90, 2, 99.9, 99.8, 99.7).equals("some string"));
+        assertFalse(new Item(5L, "Bracer", 1600.0, 1450.0, 0.90, 2, 99.9, 99.8, 99.7).equals(new Object()));
+        assertFalse(new Item(5L, "Bracer", 1600.0, 1450.0, 0.90, 2, 99.9, 99.8, 99.7).equals(null));
+    }
+
+}
