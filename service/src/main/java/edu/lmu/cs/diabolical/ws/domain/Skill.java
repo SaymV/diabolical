226d1c3e4956346f6b874154bc29ae8f9735daf8
caiomelo <caio.compufc@gmaoil.com>
2012-11-06 13:54:53 -0800

    Quest and Skill added
    	renamed:    doc/design/caio_db_scheme.txt -> doc/design/caio_db_schema.txt
    	new file:   service/src/main/java/edu/lmu/cs/diabolical/ws/domain/Quest.java
    	new file:   service/src/main/java/edu/lmu/cs/diabolical/ws/domain/Skill.java

---
+++ service/src/main/java/edu/lmu/cs/diabolical/ws/domain/Skill.java
@@ -0,0 +1,50 @@
+package edu.lmu.cs.diabolical.ws.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * A skill is a special ability from a character.
+ */
+@Entity
+@XmlRootElement
+public class Skill {
+
+    private Long id;
+    private String description;
+
+    public Skill() {
+        // No-arg constructor
+    }
+
+    public Skill(Long id, String description) {
+        this.id = id;
+        this.description = description;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @XmlAttribute
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Lob
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+}
