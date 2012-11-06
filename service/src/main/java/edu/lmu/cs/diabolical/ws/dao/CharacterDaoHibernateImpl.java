42c23b0f8753fd344eb8a464f4eb90fd9f6fefc7
Tyler Nichols <tyler.n.89@gmail.com>
2012-11-06 14:46:31 -0800

    Added stubbed out CharacterDaoHibernateImpl.

---
+++ service/src/main/java/edu/lmu/cs/diabolical/ws/dao/CharacterDaoHibernateImpl.java
@@ -0,0 +1,27 @@
+package edu.lmu.cs.diabolical.ws.dao;
+
+import java.util.List;
+
+public class CharacterDaoHibernateImpl {
+
+    public List<Character> getCharactersByQuery(String query) {
+        return null;
+    }
+    
+    public Character getCharacterById(Integer id) {
+        // Call getCharactersByQuery with just an id in the query
+        return null;
+    }
+    
+    public void deleteCharacter(Integer id) {
+        return;
+    }
+    
+    public void updateCharacter(Character c) {
+        return;
+    }
+    
+    public void createCharacter(Character c) {
+        return;
+    }
+}
