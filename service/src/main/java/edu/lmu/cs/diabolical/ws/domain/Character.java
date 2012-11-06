307733f8e3741649626065dd3f5c38c985f43ee8
Tyler Nichols <tyler.n.89@gmail.com>
2012-11-06 15:26:29 -0800

    Updated Character domain object and added current domain object classes to the JSON Context Resolver class for JAXB.

--- service/src/main/java/edu/lmu/cs/diabolical/ws/domain/Character.java
+++ service/src/main/java/edu/lmu/cs/diabolical/ws/domain/Character.java
@@ -13,75 +13,87 @@ public class Character {
     private List<Item> items;
     private List<Skill> skills;
     private List<Quest> accomplishedQuests;
-    
+
+    public void addAccomplishedQuest(Quest q) {
+        this.accomplishedQuests.add(q);
+    }
+
+    public void addItem(Item i) {
+        this.items.add(i);
+    }
+
+    public void addSkill(Skill s) {
+        this.skills.add(s);
+    }
+
     public Integer getId() {
         return id;
     }
-    
+
     public void setId(Integer id) {
         this.id = id;
     }
-    
+
     public String getName() {
         return name;
     }
-    
+
     public void setName(String name) {
         this.name = name;
     }
-    
+
     public Integer getGender() {
         return gender;
     }
-    
+
     public void setGender(Integer gender) {
         this.gender = gender;
     }
-    
+
     public String getClassType() {
         return classType;
     }
-    
+
     public void setClassType(String classType) {
         this.classType = classType;
     }
-    
+
     public Integer getLevel() {
         return level;
     }
-    
+
     public void setLevel(Integer level) {
         this.level = level;
     }
-    
+
     public Long getMoney() {
         return money;
     }
-    
+
     public void setMoney(Long money) {
         this.money = money;
     }
-    
+
     public List<Item> getItems() {
         return items;
     }
-    
+
     public void setItems(List<Item> items) {
         this.items = items;
     }
-    
+
     public List<Skill> getSkills() {
         return skills;
     }
-    
+
     public void setSkills(List<Skill> skills) {
         this.skills = skills;
     }
-    
+
     public List<Quest> getAccomplishedQuests() {
         return accomplishedQuests;
     }
-    
+
     public void setAccomplishedQuests(List<Quest> accomplishedQuests) {
         this.accomplishedQuests = accomplishedQuests;
     }
--- service/src/main/java/edu/lmu/cs/diabolical/ws/resource/NaturalJsonContextResolver.java
+++ service/src/main/java/edu/lmu/cs/diabolical/ws/resource/NaturalJsonContextResolver.java
@@ -10,6 +10,10 @@ import javax.xml.bind.JAXBContext;
 import com.sun.jersey.api.json.JSONConfiguration;
 import com.sun.jersey.api.json.JSONJAXBContext;
 
+import edu.lmu.cs.diabolical.ws.domain.Item;
+import edu.lmu.cs.diabolical.ws.domain.Quest;
+import edu.lmu.cs.diabolical.ws.domain.Skill;
+
 /**
  * A provider which ensures that JSON content is generated using the <em>natural</em> style,
  * rather than the default <em>mapped</em> style.
@@ -21,19 +25,11 @@ public class NaturalJsonContextResolver implements ContextResolver<JAXBContext> 
 
     private final Class<?>[] types = {
 
-/*
         // Domain classes requiring JSON serialization.
-        Event.class,
-        GPA.class,
-        Grant.class,
-        Major.class,
-        Role.class,
-        Student.class,
-        StudentRecord.class,
-        Term.class,
-        User.class,
-        UserRole.class
-*/
+        Character.class,
+        Item.class,
+        Quest.class,
+        Skill.class
     };
 
     private final List<Class<?>> typeList = Arrays.asList(types);
