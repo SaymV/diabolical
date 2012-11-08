f543af263d7b0659df108bea7c12d374587aa041
Eric Jaso <ericjaso@gmail.com>
2012-11-08 10:46:50 -0800

    Startings of AccountTest.

--- service/src/test/java/edu/lmu/cs/diabolical/ws/domain/UserTest.java
+++ service/src/test/java/edu/lmu/cs/diabolical/ws/domain/UserTest.java
@@ -20,5 +20,20 @@ public class UserTest {
         assertThat(u.getGender(), is(Gender.MALE));
         assertThat(u.getAccounts(), is(sampleAccounts));
     }
+    
+    @Test
+    public void testUserSetters() {
+        User u = new User(20, "upswimsdn", Gender.MALE, sampleAccounts);
+        List<Account> newAccounts = new ArrayList<Account>();
+        newAccounts.add(new Account());
+        u.setId(55);
+        u.setGender(Gender.FEMALE);
+        u.setUsername("nope");
+        u.setAccounts(newAccounts);
+        assertThat(u.getId(), is(55));
+        assertThat(u.getGender(), is(Gender.FEMALE));
+        assertThat(u.getUsername(), is("nope"));
+        assertThat(u.getAccounts().size(), is(1));
+    }
 
 }
