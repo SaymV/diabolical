6ef9f4a2da48862ae2c8fad1307241e3e61fb967
Eric Jaso <ericjaso@gmail.com>
2012-11-13 00:56:23 -0800

    Created AccountDaoHibernateImpl stub.

---
+++ service/src/main/java/edu/lmu/cs/diabolical/ws/dao/AccountDaoHibernateImpl.java
@@ -0,0 +1,39 @@
+package edu.lmu.cs.diabolical.ws.dao;
+
+import java.util.List;
+
+import edu.lmu.cs.diabolical.ws.domain.Account;
+
+public class AccountDaoHibernateImpl extends HibernatedaoSupport implements AccountDao{
+
+    @Override
+    public Account findUserById(Integer id) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public List<Account> getAccountsByQuery(String query) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void deleteAccount(Account a) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void createOrUpdateAccount(Account a) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void createAccount(Account a) {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
