4d2c62d8572675703511abeb2bd7fca0327b0245
Andrew Won <andyjwon@gmail.com>
2012-11-29 07:39:01 -0800

    Connected all elements for Item spawner from dao through resource

--- service/src/main/java/edu/lmu/cs/diabolical/ws/service/ItemServiceImpl.java
+++ service/src/main/java/edu/lmu/cs/diabolical/ws/service/ItemServiceImpl.java
@@ -1,23 +1,35 @@
 package edu.lmu.cs.diabolical.ws.service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import edu.lmu.cs.diabolical.ws.dao.ItemDao;
+import edu.lmu.cs.diabolical.ws.dao.ItemTemplateDao;
 import edu.lmu.cs.diabolical.ws.domain.Item;
 import edu.lmu.cs.diabolical.ws.domain.ItemTemplate;
+import edu.lmu.cs.diabolical.ws.domain.util.ItemGenerator;
 
 public class ItemServiceImpl extends AbstractService implements ItemService {
 
     private ItemDao itemDao;
+    private ItemTemplateDao itemTemplateDao;
 
-    public ItemServiceImpl(ItemDao itemDao) {
+    public ItemServiceImpl(ItemDao itemDao, ItemTemplateDao itemTemplateDao) {
         this.itemDao = itemDao;
+        this.itemTemplateDao = itemTemplateDao;
     }
 
     @Override
     public List<Item> getItems(String query, Integer minlevel, Integer maxlevel, int skip, int max) {
         getLogger().debug("getItems");
         return itemDao.getItems(query, minlevel, maxlevel, skip, max);
+    }
+
+    @Override
+    public Item getItemById(Long id) {
+        getLogger().debug("getItemById");
+        return itemDao.getItemById(id);
     }
 
     @Override
@@ -33,37 +45,29 @@ public class ItemServiceImpl extends AbstractService implements ItemService {
     }
 
     @Override
-    public Item getItemById(Long id) {
-        getLogger().debug("getItemById");
-        return itemDao.getItemById(id);
-    }
-
-    @Override
     public Item getSpawnedItem(Integer level, String slot) {
         getLogger().debug("getSpawnedItem");
-        // TODO Auto-generated method stub
-        return null;
+        ArrayList<ItemTemplate> itemTemplates = new ArrayList<ItemTemplate>(itemTemplateDao.getItemTemplates(level,
+                slot));
+        return ItemGenerator.generateRandomItem(itemTemplates.get(new Random().nextInt(itemTemplates.size())));
     }
 
     @Override
     public ItemTemplate getItemTemplateById(Long id) {
         getLogger().debug("getItemTemplateById");
-        // TODO Auto-generated method stub
-        return null;
+        return itemTemplateDao.getItemTemplateById(id);
     }
 
     @Override
     public ItemTemplate createItemTemplate(ItemTemplate itemTemplate) {
         getLogger().debug("createItemTemplate");
-        // TODO Auto-generated method stub
-        return null;
+        return itemTemplateDao.createItemTemplate(itemTemplate);
     }
 
     @Override
     public void createOrUpdateItemTemplate(ItemTemplate itemTemplate) {
         getLogger().debug("createOrUpdateItemTemplate");
-        // TODO Auto-generated method stub
-
+        itemTemplateDao.createOrUpdateItemTemplate(itemTemplate);
     }
 
 }
