cefb99e52a5ed7784b18c646f1a6595615f94c19
Tyler Nichols <tyler.n.89@gmail.com>
2012-12-05 19:40:09 -0800

    Stubbed out Character interface, resource, and the PATCH class.

--- service/src/main/java/edu/lmu/cs/diabolical/ws/resource/CharacterResource.java
+++ service/src/main/java/edu/lmu/cs/diabolical/ws/resource/CharacterResource.java
@@ -1,5 +1,56 @@
 package edu.lmu.cs.diabolical.ws.resource;
 
-public interface CharacterResource {
 
+import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+public interface CharacterResource {
+    @GET
+    @Path("/characters/{id}")
+    public Character getCharacterById(@PathParam("id") String id);
+    
+    @GET
+    @Path("/characters")
+    public List<Character> getCharactersByQuery(
+            @DefaultValue("") @QueryParam("name") String name,
+            @DefaultValue("") @QueryParam("class") String className,
+            @DefaultValue("") @QueryParam("gender") String gender,
+            @DefaultValue("") @QueryParam("minLevel") String minLevel,
+            @DefaultValue("") @QueryParam("maxLevel") String maxLevel,
+            @DefaultValue("") @QueryParam("skill") String skill);
+    
+    @GET
+    @Path("/characters/spawners")
+    public Character spawnRandomCharacter();
+    
+    @DELETE
+    @Path("/characters/{id}")
+    public Response deleteCharacterById(@PathParam("id") String id);
+    
+    @PUT
+    @Path("/characters/{id}")
+    public Character updateCharacterById(@PathParam("id") String id);
+    
+    @PATCH
+    @Path("/characters/{id}")
+    public Character updateCharacterByIdWithSpecifiedFields(@PathParam("id") String id);
+    
+    @POST
+    @Path("/characters")
+    public Response createCharacter(Character c);
+    
 }
--- service/src/main/java/edu/lmu/cs/diabolical/ws/resource/CharacterResourceImpl.java
+++ service/src/main/java/edu/lmu/cs/diabolical/ws/resource/CharacterResourceImpl.java
@@ -1,5 +1,17 @@
 package edu.lmu.cs.diabolical.ws.resource;
 
+import java.util.List;
+
+import javax.ws.rs.DELETE;
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Response;
+
 import edu.lmu.cs.diabolical.ws.service.CharacterService;
 
 public class CharacterResourceImpl extends AbstractResource implements CharacterResource {
@@ -8,5 +20,66 @@ public class CharacterResourceImpl extends AbstractResource implements Character
 
     public CharacterResourceImpl(CharacterService characterService) {
         this.characterService = characterService;
+    }
+
+    @Override
+    @GET
+    @Path("/characters/{id}")
+    public Character getCharacterById(@PathParam("id") String id) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    @GET
+    @Path("/characters")
+    public List<Character> getCharactersByQuery(@DefaultValue("") @QueryParam("name") String name,
+            @DefaultValue("") @QueryParam("class") String className,
+            @DefaultValue("") @QueryParam("gender") String gender,
+            @DefaultValue("") @QueryParam("minLevel") String minLevel,
+            @DefaultValue("") @QueryParam("maxLevel") String maxLevel,
+            @DefaultValue("") @QueryParam("skill") String skill) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    @GET
+    @Path("/characters/spawners")
+    public Character spawnRandomCharacter() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    @DELETE
+    @Path("/characters/{id}")
+    public Response deleteCharacterById(@PathParam("id") String id) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    @PUT
+    @Path("/characters/{id}")
+    public Character updateCharacterById(@PathParam("id") String id) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    @PATCH
+    @Path("/characters/{id}")
+    public Character updateCharacterByIdWithSpecifiedFields(@PathParam("id") String id) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    @POST
+    @Path("/characters")
+    public Response createCharacter(Character c) {
+        // TODO Auto-generated method stub
+        return null;
     }
 }
