ae5ad548f73021613a599da3a28226b5ca71eaba
Eric Jaso <ericjaso@gmail.com>
2012-12-12 23:07:28 -0800

    Test coverage for AccountService.

--- service/src/main/java/edu/lmu/cs/diabolical/ws/service/AccountService.java
+++ service/src/main/java/edu/lmu/cs/diabolical/ws/service/AccountService.java
@@ -3,15 +3,16 @@ package edu.lmu.cs.diabolical.ws.service;
 import java.util.List;
 
 import edu.lmu.cs.diabolical.ws.domain.Account;
+import edu.lmu.cs.diabolical.ws.domain.Gender;
 
 public interface AccountService {
     public Account findAccountById(Long id);
-    
-    public List<Account> getAccounts(String query);
-    
+
+    public List<Account> getAccountsByQuery(Gender gender, String login, String first, String last, int skip, int max);
+
     public void deleteAccount(Account a);
-    
+
     public void createAccount(Account a);
-    
+
     public void createOrUpdateAccount(Account a);
 }
