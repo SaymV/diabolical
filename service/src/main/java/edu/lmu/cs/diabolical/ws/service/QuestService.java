078cb712291fc932a9ed7a7a1741347079f86b9a
caiomelo <caio.compufc@gmail.com>
2013-01-31 12:19:23 -0800

    Interfaces QuestService and SkillService created.

---
+++ service/src/main/java/edu/lmu/cs/diabolical/ws/service/QuestService.java
@@ -0,0 +1,28 @@
+package edu.lmu.cs.diabolical.ws.service;
+
+import java.util.List;
+
+import edu.lmu.cs.diabolical.ws.domain.Quest;
+
+public interface QuestService {
+	    
+    /*
+	 *  Returns the quest with the given id, or null if there's no quest with such id.
+	 */	    
+    Quest getQuestById(Long id);
+	    
+	/*
+	 * Saves the given quest, which should have a null id.
+	 */
+	Quest createQuest(Quest quest);
+	   	     
+	/*
+	 * Updates or saves the given quest, which should have a non-null id.
+	 */
+	void createOrUpdateQuest(Quest quest);
+	   
+	/*
+	 * Deletes given quest.
+	 */
+	void deleteQuest(Quest quest);
+}
---
+++ service/src/main/java/edu/lmu/cs/diabolical/ws/service/SkillService.java
@@ -0,0 +1,28 @@
+package edu.lmu.cs.diabolical.ws.service;
+
+import java.util.List;
+
+import edu.lmu.cs.diabolical.ws.domain.Skill;
+
+public interface SkillService {
+	
+    /*
+	 *  Returns the skill with the given id, or null if there's no skill with such id.
+	 */	    
+    Skill getSkillById(Long id);
+		    
+    /*
+     * Saves the given skill, which should have a null id.
+     */
+    Skill createSkill(Skill skill);
+		   	     
+    /*
+     * Updates or saves the given skill, which should have a non-null id.
+     */
+    void createOrUpdateSkill(Skill skill);
+		   
+    /*
+     * Deletes given skill.
+     */
+    void deleteSkill(Skill skill);
+}
