31138e348637becb7bc878d5d725306173bb72f8
caiomelo <caio.compufc@gmail.com>
2013-02-02 23:01:06 -0800

    Implementation of Skill's DAO updated.

--- service/src/main/java/edu/lmu/cs/diabolical/ws/dao/SkillDaoHibernateImpl.java
+++ service/src/main/java/edu/lmu/cs/diabolical/ws/dao/SkillDaoHibernateImpl.java
@@ -6,25 +6,29 @@ import edu.lmu.cs.diabolical.ws.domain.Skill;
 
 public class SkillDaoHibernateImpl extends HibernateDaoSupport implements SkillDao{
 	
+	@Override
     public Skill getSkillById(Long id){
     	
         return getHibernateTemplate().get(Skill.class, id);
     	 
     }
-		    
+	
+	@Override
     public Skill createSkill(Skill skill){
     	
     	getHibernateTemplate().save(skill);
     	return skill;
     	
     }
-		   	     
+		   	 
+	@Override
     public void createOrUpdateSkill(Skill skill){
     	
     	getHibernateTemplate().saveOrUpdate(skill);
     	
     }
 		   
+	@Override
     public void deleteSkill(Skill skill){
     	
     	getHibernateTemplate().delete(skill);
