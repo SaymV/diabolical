e2ef455d877e37196a46dd0a510758691e0b1e32
dondi <dondi@lmu.edu>
2013-10-17 01:04:44 -0700

    Implement CORS filter.

---
+++ service/src/main/java/edu/lmu/cs/diabolical/ws/util/CORSFilter.java
@@ -0,0 +1,18 @@
+package edu.lmu.cs.diabolical.ws.util;
+
+import com.sun.jersey.spi.container.ContainerRequest;
+import com.sun.jersey.spi.container.ContainerResponse;
+import com.sun.jersey.spi.container.ContainerResponseFilter;
+
+public class CORSFilter implements ContainerResponseFilter {
+
+    @Override
+    public ContainerResponse filter(ContainerRequest containerRequest, ContainerResponse containerResponse) {
+        containerResponse.getHttpHeaders().putSingle("Access-Control-Allow-Origin", "*");
+        containerResponse.getHttpHeaders().putSingle("Access-Control-Allow-Credentials", "true");
+        containerResponse.getHttpHeaders().putSingle("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, OPTIONS, HEAD");
+        containerResponse.getHttpHeaders().putSingle("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With");
+        return containerResponse;
+    }
+
+}
